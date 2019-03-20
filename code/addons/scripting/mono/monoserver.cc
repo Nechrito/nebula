b360a7618a8b14a24f2d9e227f706df0025a4252
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2019-03-20 17:10:17 +0100

    [scripting] Started working on experimental support for mono runtime and csharp scripting.

---
+++ code/addons/scripting/mono/monoserver.cc
@@ -0,0 +1,129 @@
+//------------------------------------------------------------------------------
+//  monoserver.cc
+//  (C) 2019 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#define NOMINMAX
+#include "foundation/stdneb.h"
+#include "scripting/mono/monoserver.h"
+#include "io/ioserver.h"
+#include "io/textreader.h"
+
+using namespace IO;
+
+namespace Scripting
+{
+__ImplementClass(Scripting::MonoServer, 'MONO', Scripting::ScriptServer);
+__ImplementSingleton(Scripting::MonoServer);
+
+using namespace Util;
+using namespace IO;
+
+//------------------------------------------------------------------------------
+/**
+*/
+MonoServer::MonoServer()
+{
+    __ConstructSingleton;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+MonoServer::~MonoServer()
+{
+    if (this->IsOpen())
+    {
+        this->Close();
+    }
+    __DestructSingleton;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+MonoServer::Open()
+{
+    n_assert(!this->IsOpen());    
+    if (ScriptServer::Open())
+    {
+		// Intialize JIT runtime
+		this->domain = mono_jit_init("Nebula Scripting Subsystem");
+		if (!domain)
+		{
+			n_error("Failed to initialize Mono JIT runtime!");
+		}
+
+		// setup executable
+
+		IO::URI uri = IO::URI("scr:test.dll");
+		Util::String path = uri.AsString();
+
+		MonoAssembly *assembly;
+		assembly = mono_domain_assembly_open(domain, path.AsCharPtr());
+		if (!assembly)
+			n_error("Error loading mono assembly!");
+
+		char* argc[1] = { "test.dll" };
+
+		int retval = mono_jit_exec(domain, assembly, 1, argc);
+
+		n_printf("mono returned %i\n", retval);
+
+        return true;
+    }
+    return false;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+MonoServer::Close()
+{
+    n_assert(this->IsOpen());    
+    
+    // this will unregister all commands
+    ScriptServer::Close();
+
+    // shutdown mono runtime
+	mono_jit_cleanup(this->domain);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+MonoServer::Eval(const String& str)
+{
+    n_assert(this->IsOpen());    
+    n_assert(str.IsValid());
+
+	return false;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+    Evaluate script in file
+*/
+bool
+MonoServer::EvalFile(const IO::URI& file)
+{
+    n_assert(this->IsOpen());
+    auto stream = IO::IoServer::Instance()->CreateStream(file);
+    Ptr<TextReader> reader = IO::TextReader::Create();
+    reader->SetStream(stream);
+    if (reader->Open())
+    {
+        Util::String str = reader->ReadAll();
+        reader->Close();
+        return this->Eval(str);
+    }
+    return false;
+}
+
+
+
+} // namespace Scripting
---
+++ code/addons/scripting/mono/monoserver.h
@@ -0,0 +1,40 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Scripting::MonoServer
+  
+    Mono backend for the Nebula scripting subsystem.
+        
+    (C) 2019 Individual contributors, see AUTHORS file
+*/
+#include "scripting/scriptserver.h"
+#include "util/string.h"
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+
+//------------------------------------------------------------------------------
+namespace Scripting
+{
+class MonoServer : public ScriptServer
+{
+    __DeclareClass(MonoServer);
+    __DeclareSingleton(MonoServer);
+public:
+    /// constructor
+	MonoServer();
+    /// destructor
+    virtual ~MonoServer();
+    /// open the script server
+    bool Open();
+    /// close the script server
+    void Close();
+    /// evaluate a script statement in a string
+    bool Eval(const Util::String& str);	
+    /// evaluate script in file
+    bool EvalFile(const IO::URI& file);
+private:
+	MonoDomain* domain;
+};
+
+} // namespace Scripting
+//------------------------------------------------------------------------------
